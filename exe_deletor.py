b03d2385bf3a6289347f74bda0a2d12214c8d26f
henryquu <malve1275@gmail.com>
2022-12-16 23:37:06 +0100

    Removed unneeded files

--- exe_deletor.py
+++
@@ -1,8 +0,0 @@
-import os
-
-dir_name = "C:/Users/ganja/OneDrive/Pulpit/The_C_Programming_Language"
-test = os.listdir(dir_name)
-
-for item in test:
-    if item.endswith(".exe"):
-        os.remove(os.path.join(dir_name, item))
--- file_creator.py
+++
@@ -1,9 +0,0 @@
-first_task = 1
-
-chapter = 5
-last_task = 20
-
-
-for x in range(1, last_task + 1):
-    with open(f"{chapter}-{x}.c", "w"):
-    	pass
