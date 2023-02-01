c9009fe8ad55bfa0b04b89c938d8cd6aa7a751e2
henryquu <malve1275@gmail.com>
2023-02-02 00:45:52 +0100

    5-10 done

--- 5-10.c
+++ 5-10.c
@@ -1,34 +1,62 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
-    int i = 0;
-    char *first;
-    char *second;
-    char *c;
+    char **head = argv;
+    char c;
+
+    int result = 0;
+    int index = 0;
+    int started = 0;
     while (--argc > 0) {
-        c = *++argv;
-        if (c[1] == '\0')
-            switch (c[0]) {
-                case '+':
-                case 'x':
-                case '-':
-                case '/':
-                case '%':
-                    second = *(argv - ++i);
-                    first = (argc > 1)? *(argv - ++i): "";
-                    if (i <= 2) {
-                        printf("(%s %s %s) ", first, *argv, second);
-                        i++;
-                    }
-                    else
-                        printf("%s %s ", *argv, second);
-                    break;
-                default:
-                    break;
-            }
+        c = **++argv;
+        switch (c) {
+            case '+':
+                if (started)
+                    result += atoi(*(head + --index));
+                else{
+                    started = 1;
+                    result = atoi(*(head + index)) + atoi(*(head + --index));
+                }
+                break;
+            case 'x':
+                if (started)
+                    result *= atoi(*(head + --index));
+                else{
+                    started = 1;
+                    result = atoi(*(head + index)) * atoi(*(head + --index));
+                }
+                break;
+            case '-':
+                if (started)
+                    result -= atoi(*(head + --index));
+                else{
+                    started = 1;
+                    result = atoi(*(head + index)) - atoi(*(head + --index));
+                }
+                break;
+            case '/':
+                if (started)
+                    result /= atoi(*(head + --index));
+                else{
+                    started = 1;
+                    result = atoi(*(head + index)) / atoi(*(head + --index));
+                }
+                break;
+            case '%':
+                if (started)
+                    result %= atoi(*(head + --index));
+                else{
+                    started = 1;
+                    result = atoi(*(head + index)) % atoi(*(head + --index));
+                }
+                break;
+            default:
+                index++;
+                break;
+        }
     }
-    printf("\n");
+    printf("Result: %d \n", result);
     return 0;
 }
 
