a04e004b2b8e42a6bb793aabde2f3e7bd815d590
henryquu <malve1275@gmail.com>
2023-02-21 22:02:28 +0100

    5-16

--- 5-16.c
+++ 5-16.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAXLINES 5000
 
@@ -11,15 +12,16 @@ char *lineptr[MAXLINES];
 
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
-void qsort(void *lineptr[], int left, int right, int (*comp)(void *, void *),
-           int reverse, int fold_case, int directory);
+void quick_sort(void *lineptr[], int left, int right, int (*comp)(void *, void *),
+           int reverse, int fold, int directory);
 int numcmp(const char *, const char *);
+void swap(void *v[], int, int);
 
 int main(int argc, char *argv[]){
     int nlines;
     int numeric = 0;
     int reverse = 0;
-    int fold_case = 0;
+    int fold = 0;
     int directory = 0;
 
     for (int i = 0; i < argc; i++){
@@ -28,16 +30,16 @@ int main(int argc, char *argv[]){
     else if (strcmp(argv[i], "-r") == 0)
         reverse = 1;
     else if (strcmp(argv[i], "-f") == 0)
-        fold_case = 1;
+        fold = 1;
     else if (strcmp(argv[i], "-d") == 0)
         directory = 1;
     }
 
     if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
-        qsort(
+        quick_sort(
             (void**) lineptr, 0, nlines - 1, 
             (int (*)(void*, void*))(numeric ? numcmp: strcmp), 
-            reverse, fold_case, directory);
+            reverse, fold, directory);
         writelines(lineptr, nlines);
         return 0;
     } 
@@ -47,11 +49,11 @@ int main(int argc, char *argv[]){
     }
 }
 
-void qsort(void *v[], int left, int right, int (*comp)(void *, void *), 
-           int reverse, int fold_case, int directory)
+void quick_sort(void *v[], int left, int right, int (*comp)(void *, void *), 
+           int reverse, int fold, int directory)
 {
     int i, last;
-    char folded1, folded2;
+    char *folded1, *folded2;
 
     if (left >= right)
         return;
@@ -60,35 +62,25 @@ void qsort(void *v[], int left, int right, int (*comp)(void *, void *),
     last = left;
 
     for (i = left + 1; i <= right; i++){
+        strcpy(folded1, v[i]);
+        if (CAPITAL_LETTER(folded1[0]) && fold)
+            folded1[0] += 'a' - 'A';
+        strcpy(folded2, v[left]);
+        if (CAPITAL_LETTER(folded2[0]) && fold)
+            folded2[0] += 'a' - 'A';
+
         if (directory){
-            if (!CAPITAL_LETTER(v[i]) && !SMALL_LETTER(v[i]) && !BLANK(v[i]))
+            if (!CAPITAL_LETTER(folded1[0]) && !SMALL_LETTER(folded1[0]) && !BLANK(folded1[0]))
                 continue;
-            if (!CAPITAL_LETTER(v[left]) && !SMALL_LETTER(v[left]) && !BLANK(v[left]))
+            if (!CAPITAL_LETTER(folded2[0]) && !SMALL_LETTER(folded2[0]) && !BLANK(folded2[0]))
                 continue;
         }
 
-        folded1 = (CAPITAL_LETTER(v[i]) && fold_case) ? v[i] + 'a' - 'A': v[i];
-        folded2 = (CAPITAL_LETTER(v[left]) && fold_case) ? v[left] + 'a' - 'A': v[left];
         if (reverse == 0 && (*comp)(folded1, folded2) < 0)
             swap(v, ++last, i);
         else if(reverse && (*comp)(folded1, folded2) < 0)
             swap(v, left, last);
     }
-    qsort(v, left, last - 1, comp, reverse, fold_case);
-    qsort(v, last + 1, right, comp, reverse, fold_case);
+    quick_sort(v, left, last - 1, comp, reverse, fold, reverse);
+    quick_sort(v, last + 1, right, comp, reverse, fold, reverse);
 }
-
-int numcmp(const char *s1, const char *s2){
-    double v1, v2;
-
-    v1 = atof(s1);
-    v2 = atof(s2);
-
-    if (v1 < v2)
-        return -1;
-    else if (v1 > v2)
-        return 1;
-    else
-        return 0;
-}
-
