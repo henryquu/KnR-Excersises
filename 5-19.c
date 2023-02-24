f5a26824c18e3791271d91b8a54a906b30ffb5e7
henryquu <malve1275@gmail.com>
2023-02-24 17:44:50 +0100

    doing 5-19, skipped 5-18 for now

---
+++ 5-19.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXTOKEN 100
+
+enum { NAME, PARENS, BRACKETS };
+
+int gettoken(void);
+int getch(void);
+void ungetch(int);
+
+int tokentype; 
+char token[MAXTOKEN];
+char out[1000];
+
+
+int main(){
+    int type;
+    int previous_type;
+    char temp[MAXTOKEN];
+    while (gettoken() != EOF) {
+        strcpy(out, token);
+        while ((type = gettoken()) != '\n'){
+            if (type == PARENS || type == BRACKETS){
+                if (type == PARENS && previous_type == PARENS)
+                    continue;
+                else
+                    strcat(out, token);
+            }
+            else if (type == '*') {
+                sprintf(temp, "(*%s)", out);
+                strcpy(out, temp);
+            } 
+            else if (type == NAME) {
+                sprintf(temp, "%s %s", token, out);
+                strcpy(out, temp);
+            } 
+            else
+                printf("invalid input at %s\n", token);
+            previous_type = type;
+        }
+        printf("%s\n",out);
+    }
+    return 0;
+}
+
+int gettoken(void){
+    int c;
+    char *p = token;
+
+    while ((c = getch()) == ' ' || c == '\t');
+
+    if (c == '(') {
+        if ((c = getch()) == ')') {
+            strcpy(token, "()");
+            return tokentype = PARENS;
+        } 
+        else {
+            ungetch(c);
+            return tokentype = '(';
+        }
+    } 
+    else if (c == '[') {
+        for (*p++ = c; (*p++ = getch()) != ']'; );
+            *p = '\0';
+        return tokentype = BRACKETS;
+    } 
+    else if (isalpha(c)) {
+        for (*p++ = c; isalnum(c = getch()); )
+            *p++ = c;
+        *p = '\0';
+        ungetch(c);
+        return tokentype = NAME;
+    } 
+    else
+        return tokentype = c;
+}
+
+#define BUFSIZE 100
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void){
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c){
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
