cd16aacba677ea0ac27a1dcaf768f204edc7cbd5
henryquu <henryquu@gmail.com>
2023-06-06 22:25:51 +0200

    feat: added comments, working on lines list

--- chapter_6/6-3.c
+++ chapter_6/6-3.c
@@ -11,18 +11,24 @@ struct node *addtree(struct node *, char *, int);
 void group_print(struct node *, char *);
 int getword(char *, int, FILE *);
 struct node *talloc(void);
-int compare_strings(char *, char *, int);
 int getch(FILE *);
 void ungetch(int);
 int is_noise(char *);
 
+// linked list
+struct list {
+    int val;
+    struct list *next;
+} list;
 
+// word tree
 struct node {
     char *word;
-    int **list;
+    struct list *lines;
     struct node *left;
     struct node *right;
 } node;
+
 
 
 int main(int argc, char **argv){
@@ -31,6 +37,7 @@ int main(int argc, char **argv){
 
     char word[MAXWORD];
     
+    // open and check file
     FILE *file = fopen("../utils.c", "r");
     if (!file){
         printf("No file found!\n");
@@ -38,9 +45,15 @@ int main(int argc, char **argv){
         return -1;
     }
     
-    while (getword(word, MAXWORD, file) != EOF)
+    // line counting variable
+    int line = 0;
+    // variable for new line checking
+    char output;
+    while ((output = getword(word, MAXWORD, file)) != EOF)
         if (isalpha(word[0]) && !is_noise(word))
-            root = addtree(root, word, 5);
+            root = addtree(root, word, line);
+        else if (output == '\n')
+            line++;
     
     group_print(root, NULL);
 
@@ -51,19 +64,46 @@ int main(int argc, char **argv){
 
 struct node *addtree(struct node *p, char *w, int line){
     int cond;
+    struct list *new;
 
+    // new word
     if (p == NULL) {
+        // new leaf
         p = talloc();
+        
+        // new line structure
+        new = malloc(sizeof(struct list));
+        new->val = line;
+        new->next = NULL;
+
+        // asigning values to leaf
         const char *ww = w;
         p->word = strdup(ww);
+        p->lines = new;
         p->left = p->right = NULL;
-    } 
-    else if ((cond = strcmp(w, p->word)) < 0)
+    }
+    // updating leaf lines list 
+    else if ((cond = strcmp(w, p->word)) == 0){
+        // go to the end of the list
+        while ((p->lines = p->lines->next))
+        ;
+        // create a new structure
+        new = malloc(sizeof(struct list));
+        new->val = line;
+        new->next = NULL;
+
+        // assigning new value to leaf
+        p->lines = new;
+    }
+    // word should be on the left of the current leaf
+    else if (cond < 0)
         p->left = addtree(p->left, w, line);
+    // word should be on the right
     else if (cond > 0)
         p->right = addtree(p->right, w, line);
 
     return p;
+
 }
 
 
@@ -72,10 +112,17 @@ void group_print(struct node *p, char *prev){
         // left nodes recursive
         group_print(p->left, prev);
 
-        printf("%s: ", p->word);
-        // while (p->list){
-        //     printf("%d", p->list++);
-        // }
+        // printing current leaf
+        printf("%s:", p->word);
+        // tmp struct object for deletion
+        struct list *previous_line = p->lines;
+        // itereate over list of lines, print and delete them
+        while (p->lines){
+            printf(" %d", *p->lines);
+            p->lines = p->lines->next;
+            free(previous_line);
+            previous_line = p->lines;
+        }
         printf("\n");
 
         // right recursive
@@ -101,6 +148,9 @@ int getword(char *word, int lim, FILE *file){
 
     if (c != EOF)
         *w++ = c;
+
+    if (c == '\n')
+        return c;
 
     if (!isalpha(c)) {
         prev = c;
@@ -134,8 +184,10 @@ int getword(char *word, int lim, FILE *file){
 
 
 int is_noise(char *word){
+    // array of noise words
     char *noise_words[NOISE_COUNT] = {"the", "and", "but", "so", "that"};
 
+    // compare array's words with input word
     for (int i = 0; i < NOISE_COUNT; i++)
         if (strcmp(noise_words[i], word) == 0)
             return 1;
